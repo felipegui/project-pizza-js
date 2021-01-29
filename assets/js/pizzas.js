26cf84a8d3f280bc952ce6adb3ab3d913218f679
Felipe Guimarães <fguiengenharia@gmail.com>
2021-01-29 18:43:58 -0300

    Estrutura HTML

---
+++ assets/js/pizzas.js
@@ -0,0 +1,10 @@
+let pizzaJson = [
+    {id: 1, name: 'Mussarela', img: 'assets/image/mussarela.png', price: 20.19, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 2, name: 'Calabresa', img: 'assets/image/calabresa.jpeg', price: 25.19, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 3, name: 'Quatro Queijos', img: 'assets/image/quatroqueijos.jpeg', price: 27.19, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 4, name: 'Americana', img: 'assets/image/americana', price: 20.19, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 5, name: 'Frutos do Mar', img: 'assets/image/frutosdomar.jpeg', price: 40.90, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 6, name: 'Frango com Catupiry', img: 'assets/image/frangocomcatupiry.png', price: 35.00, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 7, name: 'Portuguesa', img: 'assets/image/pizzaportuguesa.jpeg', price: 20.19, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+    {id: 8, name: 'Nutella com Ninho', img: 'assets/image/pizzadenutella.jpeg', price: 20.19, sizes: ['100g', '550g', '860g'], description: 'Lorem ipsum ultrices pharetra platea etiam porttitor mi, elementum venenatis.'},
+];
