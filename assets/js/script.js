9fd4932f5178696f776b1dbb88252a316b3ba33e
Felipe Guimarães <fguiengenharia@gmail.com>
2021-02-06 01:10:23 -0300

    Preenchendo o modal - Parte I

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const qsAll = (element)=>document.querySelectorAll(element);
 pizzaJson.map((item, index) => {
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
+
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
 
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
@@ -15,6 +17,14 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.querySelector('a').addEventListener('click', (event)=>{
         event.preventDefault();
+
+        let key = event.target.closest('.pizza-item').getAttribute('data-key');
+
+        qs('.pizzaBig img').src = pizzaJson[key].img;
+
+        qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+
+        qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         
         qs('.pizzaWindowArea').style.opacity = 0;
 
